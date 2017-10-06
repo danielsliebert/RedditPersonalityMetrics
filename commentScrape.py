15b36e2fe9dac07b849bc33f2db15027c27e5730
Daniel Sonnier <sonnier.daniel@gmail.com>
2017-10-05 23:19:42 -0700

    displays personality metrics

--- commentScrape.py
+++ commentScrape.py
@@ -1,17 +1,20 @@
 # Writes out a user's comments to a plain text file for metrics processing
 import praw
 import sys
+import json
+from os.path import join, dirname
+from watson_developer_cloud import PersonalityInsightsV3
 
 userscrape = sys.argv[1]
 
 output_handle = open(userscrape + ".txt", "w")
 
 # Create reddit instance
-r = praw.Reddit(client_id='CLIENTID',
-                client_secret='SECRET',
-                password='PASSWORD',
+r = praw.Reddit(client_id='',
+                client_secret='',
+                password='',
                 user_agent='personality metrics from reddit comments',
-                username='OBFUSCATETHIS')
+                username='')
 
 user = r.redditor(userscrape)
 # Print out all new comments (Max limit = 1000 per API)
@@ -19,3 +22,21 @@ for comment in user.comments.new(limit=None):
     output_handle.write(comment.body.encode('utf-8'))
 
 output_handle.close()
+
+
+personality_insights = PersonalityInsightsV3(
+    version='2016-10-20',
+    username='',
+    password='',
+    x_watson_learning_opt_out=True
+)
+
+
+with open(join(dirname(__file__), userscrape + '.txt')) as profile_txt:
+    profile = personality_insights.profile(
+        profile_txt.read(), consumption_preferences=True
+    )
+
+with open(userscrape + '_metrics.txt', 'w') as outfile:
+    json.dump(profile, outfile)
+
